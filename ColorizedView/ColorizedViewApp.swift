94e6a90678d6b2403cfad3b91b92f3b0263a45bb
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-06-13 13:02:23 +0500

    Initial Commit

---
+++ ColorizedView/ColorizedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedViewApp.swift
+//  ColorizedView
+//
+//  Created by Юлия Ястребова on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
